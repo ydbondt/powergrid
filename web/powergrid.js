d1210079e6b7cc4e3f9c98e85587c30d2c603aef
ccantill <christoph.cantillon@pearlchain.net>
2014-09-15 15:27:53 +0200

    Added footer & header fixing

--- web/powergrid.js
+++ web/powergrid.js
@@ -13,48 +13,64 @@
 			
 			var frozenColumnsLeft=4;
             var frozenColumnsRight=2;
+            
+            var frozenRowsTop = 2;
+            var frozenRowsBottom = 1;
 			
 			var container = $("<div class='powergrid'>");
 			var scrollingcontainer = $("<div class='scrolling'>");
-			var fixedPartLeft = $("<div class='container fixed left'>");
-			var fixedPartRight = $("<div class='container fixed right'>");
-			var scrollingPart = $("<div class='container scrolling'>");
+			var headercontainer = $("<div class='header'>");
+			var footercontainer = $("<div class='footer'>");
 			
-			// start rendering
-			for(var x = 0; x < data.length; x++) {
-				var rowFixedPartLeft = $("<div class='row fixed'>");
-				var rowFixedPartRight = $("<div class='row fixed'>");
-				var rowScrollingPart = $("<div class='row scrolling'>");
+			var fixedLeft = $();
+			var fixedRight = $();
+
+			function createRowGroup(data, start, end, container) {
+				var fixedPartLeft = $("<div class='container fixed left'>");
+				var fixedPartRight = $("<div class='container fixed right'>");
+				var scrollingPart = $("<div class='container scrolling'>");
 				
-				for(var y = 0; y < data[x].length; y++) {
-					var cell = $("<div class='cell'>");
-					cell.text(data[x][y]);
-					if(y < frozenColumnsLeft) {
-						rowFixedPartLeft.append(cell);
-					} else if(y > data[x].length - frozenColumnsRight - 1) {
-                        rowFixedPartRight.append(cell);
-                    } else {
-						rowScrollingPart.append(cell);
+				fixedLeft = fixedLeft.add(fixedPartLeft);
+				fixedRight = fixedRight.add(fixedPartRight);
+				
+				// start rendering
+				for(var x = start; x < end; x++) {
+					var rowFixedPartLeft = $("<div class='row fixed'>");
+					var rowFixedPartRight = $("<div class='row fixed'>");
+					var rowScrollingPart = $("<div class='row scrolling'>");
+					
+					for(var y = 0; y < data[x].length; y++) {
+						var cell = $("<div class='cell'>");
+						cell.text(data[x][y]);
+						if(y < frozenColumnsLeft) {
+							rowFixedPartLeft.append(cell);
+						} else if(y > data[x].length - frozenColumnsRight - 1) {
+	                        rowFixedPartRight.append(cell);
+	                    } else {
+							rowScrollingPart.append(cell);
+						}
 					}
+					
+					fixedPartLeft.append(rowFixedPartLeft);
+	                fixedPartRight.append(rowFixedPartRight);
+					scrollingPart.append(rowScrollingPart);
 				}
-				
-				fixedPartLeft.append(rowFixedPartLeft);
-                fixedPartRight.append(rowFixedPartRight);
-				scrollingPart.append(rowScrollingPart);
+	
+				container.append(fixedPartLeft).append(scrollingPart).append(fixedPartRight);
 			}
 
-			scrollingcontainer.append(fixedPartLeft).append(scrollingPart).append(fixedPartRight);
-            
-            scrollingcontainer.scroll(function(event) {
-                fixedPartLeft.css("left", scrollingcontainer.scrollLeft() + "px");
-                fixedPartRight.css("right", "-" + scrollingcontainer.scrollLeft() + "px");
-            });
+			createRowGroup(data, 0, frozenRowsTop, headercontainer);
+			createRowGroup(data, frozenRowsTop, data.length - frozenRowsBottom, scrollingcontainer);
+			createRowGroup(data, data.length - frozenRowsBottom, data.length, footercontainer);
 
+            container.append(headercontainer).append(scrollingcontainer).append(footercontainer);
 			$(this).append(container);
-
-            container.append(scrollingcontainer);
-			
-            scrollFiller.css("width", (scrollingPart[0].scrollWidth + hscroller[0].offsetWidth - scrollingPart[0].offsetWidth) + "px");
+            
+            $(".powergrid > div").scroll(function(event) {
+                $(".container.fixed.left").css("left", this.scrollLeft + "px");
+                $(".container.fixed.right").css("right", "-" + this.scrollLeft + "px");
+                $(".powergrid > div").scrollLeft(this.scrollLeft);
+            });
 		}
 	});
 })(jQuery);
