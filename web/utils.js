3d39d946452bf042182756cad227f1d7385a9f0d
ccantill <christoph.cantillon@pearlchain.net>
2014-09-18 16:41:14 +0200

    fix "this" within handleEventInAnimationFrame

--- web/utils.js
+++ web/utils.js
@@ -1,14 +1,19 @@
-define({
-    handleEventInAnimationFrame: function (event) {
-        requestAnimationFrame(function() {
-            event.data(event);
-        });
-    },
+(function(define) {
+    "use strict";
     
-    findInArray: function (array, selector) {
-        for(var x=0,l=array.length;x<l;x++) {
-            if(selector(array[x], x)) return x;
+    define({
+        handleEventInAnimationFrame: function (event) {
+            var self = this, args = arguments;
+            requestAnimationFrame(function() {
+                event.data.apply(self, args);;
+            });
+        },
+
+        findInArray: function (array, selector) {
+            for(var x=0,l=array.length;x<l;x++) {
+                if(selector(array[x], x)) return x;
+            }
+            return -1;
         }
-        return -1;
-    }
-});
+    });
+})(define);
