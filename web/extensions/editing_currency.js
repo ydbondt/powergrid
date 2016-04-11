656fa243d859d88642cac10d7a8b5103c3902792
ccantill <christoph.cantillon@pearlchain.net>
2016-04-11 09:47:13 +0200

    https://pearlchain.atlassian.net/browse/PRL-4028 Impossible to save changes on the Service tab on the first try

--- web/extensions/editing_currency.js
+++ web/extensions/editing_currency.js
@@ -7,42 +7,47 @@
  *  - currencyPrecision
  */
 
-define(['override', 'jquery', 'extensions/currencyconversion'], function(override, $, currencyconversion) {
+define(['override', 'jquery', 'extensions/currencyconversion'], function (override, $, currencyconversion) {
 
     "use strict";
-    var currencyConverter;
-    return {
-        init: function(grid, pluginOptions) {
-            currencyConverter = currencyconversion(grid.target);
-        },
-        requires: {
-            editing: {
-                editors: {
-                    currency: function(record, column, value) {
-                        var input = $("<input>").w2field('money', {
-                            autoFormat: true,
-                            currencyPrefix: '',
-                            currencyPrecision: column.precision,
-                            precision: column.precision,
-                            groupSymbol: i18n('groupingSeparator'),
-                            decimalSymbol: i18n('decimalSeparator')
-                        });
-                        var offeringCurrency = currencyConverter.getCurrencyInformation().offeringCurrencyCode;
-                        var activeCurrencyCode = currencyConverter.getCurrencyInformation().activeCurrencyCode;
-                        input.val(currencyConverter.convert(offeringCurrency, activeCurrencyCode, value));
-                        
-                        var w2 = input.data('w2field');
+    return function (grid, pluginOptions) {
+        override(grid, function ($super) {
+            $.extend(true, grid.options.extensions.editing,
+                {
+                    grid: grid,
+                    editors: {
+                        currency: function (record, column, value) {
+                            var input = $("<input>").w2field('money', {
+                                autoFormat: true,
+                                currencyPrefix: '',
+                                currencyPrecision: column.precision,
+                                precision: column.precision,
+                                groupSymbol: i18n('groupingSeparator'),
+                                decimalSymbol: i18n('decimalSeparator')
+                            });
+                            var offeringCurrency = grid.currencyConverter.getCurrencyInformation().offeringCurrencyCode;
+                            var activeCurrencyCode = grid.currencyConverter.getCurrencyInformation().activeCurrencyCode;
+                            input.val(grid.currencyConverter.convert(offeringCurrency, activeCurrencyCode, value));
+                            var w2 = input.data('w2field');
 
-                        var v = input.val.bind(input);
-                        
-                        input.val = function(x) {
-                            return currencyConverter.convert(activeCurrencyCode, offeringCurrency, w2.clean(v()));
-                        };
-                        
-                        return input;
+                            var v = input.val.bind(input);
+
+                            input.val = function (x) {
+                                return grid.currencyConverter.convert(activeCurrencyCode, offeringCurrency, w2.clean(v()));
+                            };
+
+                            return input;
+                        }
                     }
                 }
+            );
+            return {
+                init: function () {
+                    var grid = this;
+                    $super.init();
+                    grid.currencyConverter = currencyconversion(grid.target);
+                }
             }
-        }
-    };
+        });
+    }
 });
