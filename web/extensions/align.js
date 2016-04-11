ab2b948797564871b83c12f7e497209b8011b219
ccantill <christoph.cantillon@pearlchain.net>
2016-04-11 09:47:17 +0200

    PRL-5008 align numbers right

---
+++ web/extensions/align.js
@@ -0,0 +1,22 @@
+/**
+ * Enables aligning of cell contents.
+ *
+ * Usage: 
+ */
+define(['override', 'jquery', 'utils'], function(override, $) {
+    "use strict";
+
+    return function(grid, pluginOptions) {
+        override(grid, function($super) {
+            return {
+                renderCell: function renderCell(record, column, rowIdx, columnIdx) {
+                    var cell = $super.renderCell(record, column, rowIdx, columnIdx);
+                    if(column.align) {
+                        cell.addClass("pg-align-" + column.align);
+                    }
+                    return cell;
+                }
+            }
+        });
+    };
+});
