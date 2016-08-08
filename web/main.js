315be94a36d9b5704133e8289a2aeeda4d90b222
Sam De Block <sam.deblock@unifly.aero>
2016-08-08 15:16:33 +0200

    6to5

--- web/main.js
+++ web/main.js
@@ -1,14 +1,10 @@
-window.define = System.amdDefine;
-window.require = window.requirejs = System.amdRequire;
-
-import PowerGrid from './powergrid';
-import ArrayDataSource from './arraydatasource';
-import JsonDataSource from './jsondatasource';
-import GroupingDataSource from './datasources/groupingdatasource';
-import SortingDataSource from './datasources/sortingdatasource';
-
-export { PowerGrid };
-export { ArrayDataSource };
-export { JsonDataSource };
-export { GroupingDataSource };
-export { SortingDataSource };
+define(['./powergrid', './arraydatasource', './jsondatasource', './datasources/groupingdatasource', './datasources/sortingdatasource'],
+    function (PowerGrid, ArrayDataSource, JsonDataSource, GroupingDataSource, SortingDataSource) {
+        return {
+            PowerGrid: PowerGrid,
+            ArrayDataSource: ArrayDataSource,
+            JsonDataSource: JsonDataSource,
+            GroupingDataSource: GroupingDataSource,
+            SortingDataSource: SortingDataSource
+        };
+    });
